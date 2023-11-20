a39d1df5975396c69982781911b40f3de2f22104
Kamlesh Katpara <16983847+kamleshkatpara@users.noreply.github.com>
2023-11-21 01:00:27 +0530

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -71,6 +71,6 @@ sudo apt-get update -y
 sudo apt-get install -y jq
 
 local_ip="$(ip --json addr show eth0 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
-cat > /etc/default/kubelet << EOF
+sudo sh -c 'cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
-EOF
+EOF'
