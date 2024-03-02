5e982331fa50de92a1310de4c9e9c482374b665c
TechiesCamp <106984297+techiescamp@users.noreply.github.com>
2024-03-02 16:37:13 +0530

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -39,8 +39,4 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Install Claico Network Plugin Network 
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
-
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
-
-kubectl create -f custom-resources.yaml
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
