a06452ef5c13f1a338f05f25ae7731a81396bc8d
VALKIRIA ACUATICA <56233573+valkiriaaquatica@users.noreply.github.com>
2024-07-07 14:49:30 +0200

    change to variable also in master

--- scripts/master.sh
+++ scripts/master.sh
@@ -14,11 +14,14 @@ POD_CIDR="192.168.0.0/16"
 
 sudo kubeadm config images pull
 
+# detects the network interface
+interface=$(ip route | grep default | awk '{print $5}')
+
 # Initialize kubeadm based on PUBLIC_IP_ACCESS
 
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
+    MASTER_PRIVATE_IP=$(ip addr show "$interface" | awk '/inet / {print $2}' | cut -d/ -f1)
     sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
 elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
