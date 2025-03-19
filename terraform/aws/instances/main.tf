5efab3484b587593b893c4ed263e58f34772442e
TechiesCamp <106984297+techiescamp@users.noreply.github.com>
2025-03-19 16:07:45 +0530

    Update main.tf

--- terraform/aws/instances/main.tf
+++ terraform/aws/instances/main.tf
@@ -6,18 +6,18 @@ module "ec2_instance" {
   source = "../modules/ec2"
 
   instance_name  = "k8s-node"
-  ami_id         = "ami-0735c191cf914754d"
+  ami_id         = "ami-00c257e12d6828491"
   instance_type  = "t2.medium"
   key_name       = "techiescamp"
-  subnet_ids     = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
+  subnet_ids     = ["subnet-0f92233e44d3044ef", "subnet-007ab506046047319", "subnet-006538decc4e58a2e"]
   instance_count = 3
 
-  inbound_from_port  = ["0", "6443", "22", "30000"]
-  inbound_to_port    = ["65000", "6443", "22", "32768"]
-  inbound_protocol   = ["TCP", "TCP", "TCP", "TCP"]
-  inbound_cidr       = ["172.31.0.0/16", "0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"]
-  outbound_from_port = ["0"]
-  outbound_to_port   = ["0"]
-  outbound_protocol  = ["-1"]
-  outbound_cidr      = ["0.0.0.0/0"]
+inbound_from_port  = ["0", "6443", "22", "30000", "0"]
+inbound_to_port    = ["65000", "6443", "22", "32768", "65000"]
+inbound_protocol   = ["TCP", "TCP", "TCP", "TCP", "TCP"]
+inbound_cidr       = ["172.31.0.0/16", "0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0", "10.244.0.0/16"]
+outbound_from_port = ["0"]
+outbound_to_port   = ["0"]
+outbound_protocol  = ["-1"]
+outbound_cidr      = ["0.0.0.0/0"]
 }
